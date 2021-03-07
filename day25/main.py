d5a609fdbdc5d4f4ed938881d5d4501a7a378684
Lenar Gasimov <lv4262122+github@gmail.com>
2021-03-07 21:42:19 +0300

    fix: short code

--- day25/main.py
+++ day25/main.py
@@ -16,13 +16,11 @@ while len(guessed_states) < 50:
     answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct",
                                     prompt="What's another state's name?").title()
     if answer_state == "Exit":
-        missing_states = []
-        for state in all_states:
-            if state not in guessed_states:
-                missing_states.append(state)
+        missing_states = [state for state in all_states if state not in guessed_states]
         new_data = pandas.DataFrame(missing_states)
         new_data.to_csv("states_to_learn.csv")
         break
+
     if answer_state in all_states:
         guessed_states.append(answer_state)
         t = turtle.Turtle()

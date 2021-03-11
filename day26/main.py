fbcf191c6d2fa837bbd022f34cb8f97c60c8371c
Lenar Gasimov <lv4262122+github@gmail.com>
2021-03-11 13:51:24 +0300

    feat: added complete day 26
    added except KeyError

--- day26/main.py
+++ day26/main.py
@@ -10,6 +10,14 @@ phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
 
 #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 
-word = input("Enter a word: ").upper()
-output_list = [phonetic_dict[letter] for letter in word]
-print(output_list)
+def generate_phonetic():
+    word = input("Enter a word: ").upper()
+    try:
+        output_list = [phonetic_dict[letter] for letter in word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        generate_phonetic()
+    else:
+        print(output_list)
+
+generate_phonetic()

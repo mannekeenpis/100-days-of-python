42baeacd8d1c8133df46979d92bf83c877189c82
Lenar Gasimov <lv4262122+github@gmail.com>
2021-04-09 13:02:27 +0300

    feat: added complete day 59
    Created a simple blog used Flask, Jinja, Bootstrap, HTML, and CSS.

---
+++ day59/README.md
@@ -0,0 +1,7 @@
+# Day 59
+
+Created a simple blog used Flask, Jinja, Bootstrap, HTML, and CSS.
+
+## Blog Capstone Project Part 2
+
+![blog](blog.png)
---
+++ day59/upgraded blog/main.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template
+import requests
+
+posts = requests.get("https://api.npoint.io/0067e63917ca7a5034d9").json()
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def get_all_posts():
+    return render_template("index.html", all_posts=posts)
+
+
+@app.route("/post/<int:index>")
+def show_post(index):
+    requested_post = None
+    for blog_post in posts:
+        if blog_post["id"] == index:
+            requested_post = blog_post
+    return render_template("post.html", post=requested_post)
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
